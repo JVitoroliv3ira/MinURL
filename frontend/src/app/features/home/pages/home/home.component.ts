123f321aa0062b486426bc2b81d894a8395f8953
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-02-17 14:39:40 -0300

    feat: Utiliza o footer da aplicação na home

--- frontend/src/app/features/home/pages/home/home.component.ts
+++ frontend/src/app/features/home/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../../layout/header/header.component";
+import { FooterComponent } from "../../../../layout/footer/footer.component";
 
 @Component({
   selector: 'min-url-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
