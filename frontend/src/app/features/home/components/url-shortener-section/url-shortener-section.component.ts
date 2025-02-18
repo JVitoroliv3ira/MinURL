9155e090127846e722bfa79aa5c3e5cc59e0e323
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-02-18 17:15:07 -0300

    feat: Cria componente de section para encurtar links

---
+++ frontend/src/app/features/home/components/url-shortener-section/url-shortener-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'min-url-url-shortener-section',
+  imports: [],
+  templateUrl: './url-shortener-section.component.html'
+})
+export class UrlShortenerSectionComponent {
+
+}
