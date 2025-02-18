cf91af08e5f448c9ab0d8a13b0b4f9314d822ecc
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-02-18 18:06:53 -0300

    feat: Implementa card para exibir o resultado do encurtamento

---
+++ frontend/src/app/features/home/components/shortened-url-result/shortened-url-result.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'min-url-shortened-url-result',
+  imports: [],
+  templateUrl: './shortened-url-result.component.html'
+})
+export class ShortenedUrlResultComponent {
+
+}
--- frontend/src/app/features/home/pages/home/home.component.ts
+++ frontend/src/app/features/home/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../../layout/header/header.component";
 import { FooterComponent } from "../../../../layout/footer/footer.component";
 import { UrlShortenerSectionComponent } from "../../components/url-shortener-section/url-shortener-section.component";
+import { ShortenedUrlResultComponent } from "../../components/shortened-url-result/shortened-url-result.component";
 
 @Component({
   selector: 'min-url-home',
-  imports: [HeaderComponent, FooterComponent, UrlShortenerSectionComponent],
+  imports: [HeaderComponent, FooterComponent, UrlShortenerSectionComponent, ShortenedUrlResultComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
