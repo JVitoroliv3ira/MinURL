734fc09c620bbc31a80dff375444fb028b71daed
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-02-18 17:15:38 -0300

    feat: Utiliza a section de encurtar urls na home do projeto

--- frontend/src/app/features/home/pages/home/home.component.ts
+++ frontend/src/app/features/home/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../../layout/header/header.component";
 import { FooterComponent } from "../../../../layout/footer/footer.component";
+import { UrlShortenerSectionComponent } from "../../components/url-shortener-section/url-shortener-section.component";
 
 @Component({
   selector: 'min-url-home',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, UrlShortenerSectionComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
